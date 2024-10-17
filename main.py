3b5e8baee2e38bc5a12d67b1010de369ad8be7c4
Abel Gómez Méndez <abelmetaltele@gmail.com>
2024-10-17 09:07:01 -0400

    Daemon thread and docstrings

--- main.py
+++ main.py
@@ -4,13 +4,29 @@ import speedtest
 import os
 import ssl
 from urllib.request import Request, urlopen
-import requests
 import threading
 from typing import Optional
 from pydantic import BaseModel, ValidationError, Field, field_validator
 import re
 
+# Define global variables for thread handling and stop flag
+test_thread = None
+
+
 class ProxyConfig(BaseModel):
+    """
+    A configuration model for proxy settings.
+
+    Attributes:
+        proxy_user (str): The username for the proxy. Must be at least 1 character long.
+        proxy_pass (str): The password for the proxy. Must be at least 1 character long.
+        proxy_ip (str): The IP address of the proxy. Must match the format xxx.xxx.xxx.xxx where xxx is a number between 0 and 255.
+        proxy_port (int): The port number of the proxy. Must be an integer between 1 and 65535.
+
+    Validations:
+        - `proxy_ip`: Validates that the IP address format is correct and that each octet is between 0 and 255.
+        - `proxy_port`: Validates that the port number is between 1 and 65535.
+    """
     proxy_user: str = Field(..., min_length=1)
     proxy_pass: str = Field(..., min_length=1)
     proxy_ip: str = Field(..., pattern=r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$')
@@ -34,11 +50,23 @@ class ProxyConfig(BaseModel):
 
 
 def proxy_config() -> Optional[ProxyConfig]:
+    """
+    Prompts the user for proxy configuration details and validates the input.
+
+    This function uses dialog boxes to collect the proxy username, password,
+    IP address, and port number from the user. It handles user cancellation and
+    validation errors, returning an instance of ProxyConfig if the input is valid
+    or None if the input is canceled or invalid.
+
+    Returns:
+        Optional[ProxyConfig]: An instance of ProxyConfig if the input is valid;
+        None if the user cancels the input or if validation fails.
+    """
     proxy_user = simpledialog.askstring("Proxy Configuration", "Enter proxy username:")
     if proxy_user is None:  # Handle cancel button press
         append_debug_message("Proxy configuration canceled by user.")
         return None
-
+    
     proxy_pass = simpledialog.askstring("Proxy Configuration", "Enter proxy password:", show='*')
     if proxy_pass is None:  # Handle cancel button press
         append_debug_message("Proxy configuration canceled by user.")
@@ -89,28 +117,51 @@ def append_debug_message(message: str) -> None:
 
 def check_internet_connection() -> bool:
     """
-    Checks if there is an active internet connection by trying to reach Google.
-    
+    Checks the internet connection by attempting to connect to Google.
+
+    This function sends a request to Google's homepage and checks the response status.
+    It logs the attempt and the response status. If the connection is successful and
+    the status code is 200, it returns True. Otherwise, it catches any exceptions,
+    logs the error, and returns False.
+
     Returns:
-        bool: True if internet connection is available, False otherwise.
+        bool: True if the connection is successful (status code 200), False otherwise.
     """
     try:
-        append_debug_message("Intentando conectar a Google...")
+        append_debug_message("Trying to connect to Google to identify the type of internet connection.")
         request = Request('https://www.google.com', headers={'User-Agent': 'Mozilla/5.0'})
         with urlopen(request, timeout=5) as response:
-            append_debug_message(f"Estado de la respuesta: {response.status}")
+            append_debug_message(f"Response status: {response.status}")
             return response.status == 200
     except Exception as e:
-        append_debug_message(f"Error al intentar conectar: {e}")
+        append_debug_message(f"Error trying to connect: {e}")
         return False
 
 
 def check_proxy() -> Optional[str]:
+    """
+    Checks for existing proxy settings and configures a proxy if necessary.
+
+    This function first checks the environment variables for existing HTTP and HTTPS proxy settings.
+    If any proxy settings are detected, it logs this information and returns the HTTP or HTTPS proxy.
+    
+    If no proxy is set and an internet connection is available, it logs that a direct connection is
+    available and returns None. If no internet connection is detected, it prompts the user to
+    configure a proxy using the `proxy_config` function.
+
+    If the user cancels the proxy configuration, it logs this event and returns None. Otherwise, 
+    it sets the HTTP and HTTPS proxy environment variables using the provided user credentials and IP address,
+    and logs that the proxy has been configured.
+
+    Returns:
+        Optional[str]: The configured HTTP proxy if successful, or None if no proxy is needed or configuration was canceled.
+    """
+
     http_proxy = os.environ.get('http_proxy')
     https_proxy = os.environ.get('https_proxy')
 
     if http_proxy or https_proxy:
-        append_debug_message(f"Proxy settings detected:\nhttp_proxy: {http_proxy}\nhttps_proxy: {https_proxy}")
+        append_debug_message(f"Proxy settings detected")
         return http_proxy or https_proxy
 
     if check_internet_connection():
@@ -127,9 +178,8 @@ def check_proxy() -> Optional[str]:
     os.environ['http_proxy'] = f"http://{proxy_config_instance.proxy_user}:{proxy_config_instance.proxy_pass}@{proxy_config_instance.proxy_ip}:{proxy_config_instance.proxy_port}"
     os.environ['https_proxy'] = os.environ['http_proxy']
 
-    append_debug_message(f"Proxy configured:\nhttp_proxy: {os.environ['http_proxy']}\nhttps_proxy: {os.environ['https_proxy']}")
+    append_debug_message(f"Proxy configured")
     return os.environ['http_proxy']
-
 
 
 def update_progress(value: int) -> None:
@@ -147,8 +197,25 @@ def update_progress(value: int) -> None:
 
 def speed_test() -> None:
     """
-    Performs the internet speed test, updating the progress bar as the test progresses.
-    Displays download and upload speed results after the test.
+    Conducts an internet speed test, updating the progress bar throughout the process.
+
+    This function initializes the speed test, fetching available servers and selecting the best one
+    to ensure accurate results. It measures both download and upload speeds, converting the results 
+    to Mbps for easier interpretation.
+
+    The function updates the progress bar at various stages of the test: 
+    - 0%: Starting the speed test
+    - 20%: Fetching servers
+    - 40%: Selecting the best server
+    - 60%: Performing the download test
+    - 80%: Performing the upload test
+    - 100%: Completing the speed test
+
+    After completing the tests, it displays the results for download and upload speeds in the user interface,
+    and logs the results for debugging purposes.
+
+    Returns:
+        None: This function does not return a value but updates the application state and UI elements directly.
     """
     try:
         append_debug_message("Starting speed test...")
@@ -182,16 +249,40 @@ def speed_test() -> None:
     except Exception as e:
         messagebox.showerror("Error", str(e))
         append_debug_message(f"Error during speed test: {e}")
+    finally:
+        global test_thread
+        test_thread = None  # Liberar el hilo después de finalizar el test, para permitir nuevas pruebas
 
 
 def get_speed() -> None:
+    """
+    Initiates an internet speed test, checking for proxy settings before execution.
+
+    This function first checks if a speed test is already in progress. If so, it logs a message and 
+    prevents the initiation of a new test. It then verifies the presence of proxy settings and logs 
+    the detected proxy.
+
+    If no proxy is detected or if the proxy configuration process is canceled, the function starts 
+    a new thread to run the speed test without proxy interference. The thread is set as a daemon to 
+    ensure it does not block the application from closing.
+
+    Returns:
+        None: This function does not return a value but initiates the speed test process in a separate thread.
+    """
+    global test_thread
     try:
+        if test_thread and test_thread.is_alive():
+            append_debug_message("A speed test is already in progress. Please wait.")
+            return  # Evitar iniciar otro test mientras uno está en curso
         proxy = check_proxy()
         append_debug_message(f"Proxy detected: {proxy}")
 
         if proxy is None:  # Si no hay proxy o se cancela, seguimos con la prueba
             append_debug_message("No proxy detected, performing speed test without proxy.")
-            threading.Thread(target=speed_test).start()
+            # Usamos test_thread para manejar el hilo de la prueba
+            test_thread = threading.Thread(target=speed_test)
+            test_thread.daemon = True  # Configurar el hilo como demonio
+            test_thread.start()
             return
 
         # Si se detecta proxy, configurar e intentar la autenticación
@@ -209,7 +300,9 @@ def get_speed() -> None:
             if response.status != 200:
                 raise Exception("Proxy authentication failed. Please check your credentials and try again.")
 
-        threading.Thread(target=speed_test).start()
+            # Usamos test_thread para manejar el hilo de la prueba
+        test_thread = threading.Thread(target=speed_test)
+        test_thread.start()
 
     except ConnectionError as e:
         messagebox.showerror("Connection Error", str(e))
@@ -222,15 +315,35 @@ def get_speed() -> None:
             messagebox.showerror("Error", str(e))
         append_debug_message(f"Error: {e}")
 
+def on_close() -> None:
+    """
+    Handles the closing of the application.
+
+    This function is triggered when the application is requested to close. 
+    It performs two main actions:
+    1. Quits the application, which initiates the closing process.
+    2. Destroys the main application window, freeing up resources.
+
+    Returns:
+        None: This function does not return a value but ensures proper termination of the application.
+    """
+    app.quit()  # Cerrar la aplicación
+    app.destroy()  # Destruir la ventana
+
 
 
 def create_window() -> Optional[tk.Tk]:
     """
-    Creates the main window and sets up the user interface elements including the progress bar,
-    result display label, and speed test button. Applies a modern UI design using colors and fonts.
-    
+    Creates and configures the main application window for the Internet Speed Test application.
+
+    This function initializes a Tkinter window with a specified size, title, and icon. 
+    It sets the background color and adds various widgets, including buttons, labels, 
+    and a progress bar, to facilitate the speed testing process. Additionally, 
+    it captures the close event to ensure proper application termination.
+
     Returns:
-        Optional[tk.Tk]: The root window object, or None if an error occurs.
+        Optional[tk.Tk]: Returns the main application window instance if created successfully; 
+                          otherwise, returns None in case of an error.
     """
     global app, frame, button, result_label, progress, percentage_label, debug_log
     try:
@@ -239,6 +352,9 @@ def create_window() -> Optional[tk.Tk]:
         app.title("Internet Speed Test")
         app.iconbitmap('icono.ico')
         app.config(bg="#1E1E2F")
+
+        # Capture the close event and call on_close function
+        app.protocol("WM_DELETE_WINDOW", on_close)
 
         frame = tk.Frame(app, bg="#1E1E2F")
         frame.pack(expand=True, pady=20)
